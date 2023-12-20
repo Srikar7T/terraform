ef37c0a3c9a2dbd1bc2e4eb7c9792951f0a822d6
Srikar N <61531041+Srikar7T@users.noreply.github.com>
2023-12-20 15:06:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "mysequel_instance" {
 resource "aws_instance" "apacheinstall_instance" {
   ami           = "ami-093467ec28ae4fe03"
   instance_type = "t2.micro"
-  key_name "testpoc"
+  key_name = "testpoc"
 
   tags = {
     Name = "apache_instance1"
