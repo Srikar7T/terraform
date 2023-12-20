fb2e6862cbdc2604bc14a10559dc2a642158c9fd
Srikar N <61531041+Srikar7T@users.noreply.github.com>
2023-12-20 13:42:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,23 @@
 provider "aws"{
     region="us-west-2"
 }
-resource "aws_instance" "mysql_instance" {
+resource "aws_instance" "mysequel_instance" {
   ami           = "ami-093467ec28ae4fe03"
   instance_type = "t2.micro"
+  key_name = "testpoc"
 
   tags = {
-    Name = "MySQL_Instance"
+    Name = "MySQL_Instance1"
   }
 }
-resource "aws_instance" "apache_instance" {
+resource "aws_instance" "apacheinstall_instance" {
   ami           = "ami-093467ec28ae4fe03"
   instance_type = "t2.micro"
+  key_name "testpoc"
 
   tags = {
-    Name = "apache-instance"
+    Name = "apache_instance1"
   }
 }
 
- 
 
